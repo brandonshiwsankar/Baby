8baf5f3cae662d581bd748b86383dd86af67b4fa
Beylull Teklay <beyluleyob@yahoo.com>
2020-11-22 20:50:53 -0500

    Homepage first commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports =
 {
     //"URI":"mongodb://localhost/test"
-    //"URI":"mongodb+srv://baby:teambaby@baby.ooti5.mongodb.net/survey?retryWrites=true&w=majority"
-    "URI":"mongodb+srv://test:test@test.rhdqt.mongodb.net/test?retryWrites=true&w=majority"
+    "URI":"mongodb+srv://baby:teambaby@baby.ooti5.mongodb.net/survey?retryWrites=true&w=majority"
+    //"URI":"mongodb+srv://test:test@test.rhdqt.mongodb.net/test?retryWrites=true&w=majority"
 }
